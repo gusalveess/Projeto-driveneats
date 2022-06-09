6d1feb69c043033156b3c49289c64da979174ae9
Gusalveess <gustaalveess1302@gmail.com>
2022-06-09 16:17:25 -0300

    version-two

---
+++ scripts.js
@@ -0,0 +1,101 @@
+function choiceyin (){
+    document.getElementById("yin").style.border= "5px #32B72F solid";
+    document.getElementById("yang").style.border= "5px white solid";
+    document.getElementById("yin-yang").style.border= "5px white solid";
+
+    document.getElementById("icon-one").style.display= "block"
+    document.getElementById("icon-two").style.display= "none"
+    document.getElementById("icon-three").style.display= "none"
+}
+
+function choiceyang (){
+    document.getElementById("yang").style.border= "5px #32B72F solid";
+    document.getElementById("yin").style.border= "5px white solid";
+    document.getElementById("yin-yang").style.border= "5px white solid";
+
+    document.getElementById("icon-one").style.display= "none"
+    document.getElementById("icon-two").style.display= "block"
+    document.getElementById("icon-three").style.display= "none"
+
+}
+
+function choiceyinyang (){
+    document.getElementById("yin-yang").style.border= "5px #32B72F solid";
+    document.getElementById("yin").style.border= "5px white solid";
+    document.getElementById("yang").style.border= "5px white solid";
+
+    document.getElementById("icon-one").style.display= "none"
+    document.getElementById("icon-two").style.display= "none"
+    document.getElementById("icon-three").style.display= "block"
+
+}   
+
+function choiceagua (){
+    document.getElementById("agua").style.border= "5px #32B72F solid";
+    document.getElementById("coca").style.border= "5px white solid";
+    document.getElementById("cerveja").style.border= "5px white solid";
+
+    document.getElementById("icon-four").style.display= "block"
+    document.getElementById("icon-five").style.display= "none"
+    document.getElementById("icon-six").style.display= "none"
+
+}   
+
+
+function choicecoca (){
+    document.getElementById("agua").style.border= "5px white solid";
+    document.getElementById("coca").style.border= "5px #32B72F solid";
+    document.getElementById("cerveja").style.border= "5px white solid";
+
+    document.getElementById("icon-four").style.display= "none"
+    document.getElementById("icon-five").style.display= "block"
+    document.getElementById("icon-six").style.display= "none"
+
+}   
+
+
+function choicecerveja (){
+    document.getElementById("agua").style.border= "5px white solid";
+    document.getElementById("coca").style.border= "5px white solid";
+    document.getElementById("cerveja").style.border= "5px #32B72F solid";
+
+    document.getElementById("icon-four").style.display= "none"
+    document.getElementById("icon-five").style.display= "none"
+    document.getElementById("icon-six").style.display= "block"
+
+}   
+
+function choicebolo (){
+    document.getElementById("bolo").style.border= "5px #32B72F solid";
+    document.getElementById("pudim").style.border= "5px white solid";
+    document.getElementById("mousse").style.border= "5px white solid";
+
+    document.getElementById("icon-seven").style.display= "block"
+    document.getElementById("icon-eight").style.display= "none"
+    document.getElementById("icon-nine").style.display= "none"
+
+}   
+
+
+function choicepudim (){
+    document.getElementById("bolo").style.border= "5px white solid";
+    document.getElementById("pudim").style.border= "5px #32B72F solid";
+    document.getElementById("mousse").style.border= "5px white solid";
+
+    document.getElementById("icon-seven").style.display= "none"
+    document.getElementById("icon-eight").style.display= "block"
+    document.getElementById("icon-nine").style.display= "none"
+
+}   
+
+
+function choicemousse(){
+    document.getElementById("bolo").style.border= "5px white solid";
+    document.getElementById("pudim").style.border= "5px white solid";
+    document.getElementById("mousse").style.border= "5px #32B72F solid";
+
+    document.getElementById("icon-seven").style.display= "none"
+    document.getElementById("icon-eight").style.display= "none"
+    document.getElementById("icon-nine").style.display= "block"
+
+}   
